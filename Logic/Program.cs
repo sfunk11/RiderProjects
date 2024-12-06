33f41a1d0b7cc8afe4d85f90467c30f2e7b2ae1b
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2024-12-06 14:47:05 -0500

    initial commit for Logic lessons

---
+++ Logic/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections.Concurrent;
+
+
+
+bool isRainy = true;
+bool hasUmbrella = true;
+
+//LOGICIAL OPERATORS ARE || && !
+
+//VARIANTS OF OR:
+//TRUE & TRUE -> TRUE
+//TRUE & FALSE -> TRUE
+//FALSE & TRUE -> TRUE
+//FALSE & FALSE -> FALSE
+
+//AND IS ONLY TRUE WHEN BOTH ARGS ARE TRUE
+
+if (isRainy || hasUmbrella){
+    
+    
+    Console.WriteLine("I'm not gettting wet");
+}
+
+if (isRainy && !hasUmbrella)
+{
+    Console.WriteLine("I'm gettting wet");
+}
+Console.WriteLine("A OK");
+
+//Relational logic
+
+int age = 13;
+
+if (age >= 18)
+{
+    Console.WriteLine("Go Party!");
+}
+else
+{
+    Console.WriteLine("No club for you!");
+}
+
+
+Console.ReadKey();
+
+
+
+
