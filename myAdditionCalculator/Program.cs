e7e9b44a7e9e1b297c4aeb31776f7c1a73d37bf8
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2024-12-06 12:11:12 -0500

    intital commit for myAdditionCalculator

---
+++ myAdditionCalculator/Program.cs
@@ -0,0 +1,17 @@
+﻿//Simple program to add two user generated integers.
+
+Console.WriteLine("Enter a number:");
+
+string firstNumber = Console.ReadLine();
+
+Console.WriteLine("You entered " + firstNumber);
+Console.WriteLine("Enter another number: ");
+string secondNumber = Console.ReadLine();
+Console.WriteLine("You entered " + secondNumber);
+
+int firstNumberAsInt = int.Parse(firstNumber);
+int secondNumberAsInt = int.Parse(secondNumber);
+int result = firstNumberAsInt + secondNumberAsInt;
+Console.WriteLine($"{firstNumber} plus {secondNumber} equals {result}.");
+
+Console.ReadKey();
