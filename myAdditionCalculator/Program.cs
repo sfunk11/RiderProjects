d1bca04e2e2862390ddc030155ab36b55989509d
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2024-12-06 13:12:00 -0500

    added rounding for sum

--- myAdditionCalculator/Program.cs
+++ myAdditionCalculator/Program.cs
@@ -1,13 +1,16 @@
 ﻿//Simple program to add two user generated numbers..
 
 
+using System.Globalization;
+
 Console.WriteLine("Enter a number:");
-double firstNumber = double.Parse(Console.ReadLine());
+double firstNumber = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 Console.WriteLine("You entered " + firstNumber);
 Console.WriteLine("Enter another number: ");
-double secondNumber = double.Parse(Console.ReadLine());
+double secondNumber = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 Console.WriteLine("You entered " + secondNumber);
 double sum = firstNumber + secondNumber;
-Console.WriteLine($"{firstNumber} plus {secondNumber} equals {sum}.");
+sum = Math.Round(sum, 2);
+Console.WriteLine($"{firstNumber.ToString(CultureInfo.InvariantCulture)} plus {secondNumber} equals {sum}.");
 
 Console.ReadKey();
