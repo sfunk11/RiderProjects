91c096934e0b64414fad7fd10ace997fdc9c4438
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2024-12-06 15:38:43 -0500

    added Switch app

---
+++ SwitchApp/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int month = 5;
+string monthName = "";
+
+switch (month)
+{
+    case 1:
+        monthName = "January";
+        break;
+    case 2:
+        monthName = "February";
+        break;
+    case 3:
+        monthName = "March";
+        break;
+    case 4:
+        monthName = "April";
+        break;
+    case 5:
+        monthName = "May";
+        break;
+    case 6:
+        monthName = "June";
+        break;
+    default:
+        monthName = "Unknown";
+}
