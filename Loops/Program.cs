c416f4764b76193ddfd0bc317985b2b3df7e0cf0
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2024-12-07 12:44:08 -0500

    oops

---
+++ Loops/Program.cs
@@ -0,0 +1,15 @@
+﻿// basic for loop
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine("i is " + i);
+}
+
+// decrementing for loop with 1 sec delay
+
+for (int i = 10; i >= 0; i--)
+{
+    Console.WriteLine("i is " + i);
+    Thread.Sleep(1000);
+}
+
