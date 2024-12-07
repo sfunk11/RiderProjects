7305cd961a89c7578552b2a4f516cac619acc1a5
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2024-12-07 13:21:24 -0500

    added a random number guessing game with while loop

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -1,15 +1,62 @@
-﻿// basic for loop
+﻿// // basic for loop
+//
+// for (int i = 0; i < 10; i++)
+// {
+//     Console.WriteLine("i is " + i);
+// }
+//
+// // decrementing for loop with 1 sec delay
+//
+// for (int i = 10; i >= 0; i--)
+// {
+//     Console.WriteLine("i is " + i);
+//     Thread.Sleep(1000);
+// }
 
-for (int i = 0; i < 10; i++)
+// basic while loop
+//
+// int counter = 0;
+// while (counter < 10)
+// {
+//     Console.WriteLine("counter is " + counter);
+//     counter++;
+// }
+//
+
+// Console.WriteLine("Do you want to go for a run? Type Go or stay");
+// string userChoice = Console.ReadLine().ToLower();
+//
+//
+//
+// while (userChoice == "go")
+// {
+//     Console.WriteLine("Go for a mile.");
+//     Console.WriteLine("Do you want to keep going? Type go or stay");
+//     userChoice = Console.ReadLine().ToLower();
+// }
+// Console.WriteLine("All done!");
+//
+
+// guess a number app
+Random random = new Random();
+int number = random.Next(1, 101);
+int userGuess = 0;
+int counter = 0;
+while (userGuess != number)
 {
-    Console.WriteLine("i is " + i);
+    Console.WriteLine("Guess a number between 1 and 100:");
+    userGuess = int.Parse(Console.ReadLine());
+
+    if (userGuess > number)
+    {
+        Console.WriteLine("Too high, try again.");
+    }
+    else
+    {
+        Console.WriteLine("Too low, try again.");
+    }
+
+    counter++;
 }
 
-// decrementing for loop with 1 sec delay
-
-for (int i = 10; i >= 0; i--)
-{
-    Console.WriteLine("i is " + i);
-    Thread.Sleep(1000);
-}
-
+Console.WriteLine($"You guessed the number correctly! It took you {counter} tries.");
--- Loops/obj/Debug/net9.0/Loops.AssemblyInfo.cs
+++ Loops/obj/Debug/net9.0/Loops.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Loops")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ce728d7b25454e23b790b637852961671254501")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78d450daee7680c1e5d5df5cca161d5d0d273e71")]
 [assembly: System.Reflection.AssemblyProductAttribute("Loops")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Loops")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
