d0229098920ba32d6f034ba35e300f7f53c3ef20
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2024-12-12 17:11:55 -0500

    initial commit for Classes app

---
+++ ClassesApp/Car.cs
@@ -0,0 +1,50 @@
+
+namespace ClassesApp;
+
+internal class Car
+{
+    //member variable
+    private string _model = "";
+    private string _make = "";
+    private bool _isLuxury = false;
+    
+    //Constructor
+    
+    public Car()
+    {
+        Console.WriteLine("An Object of Car has been created");
+    }
+
+    public Car( string make, string model, bool luxury)
+    {
+        Model = model;
+        Make = make;
+        _isLuxury = luxury;
+        Console.WriteLine($"{Make} {Model} has been created");
+    }
+
+    //Properties
+    public string Model
+    {
+        get
+        {
+            if (IsLuxury)
+            {
+                return _model + " Luxury Edition";
+            }   
+        }
+        set => _model = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public string Make
+    {
+        get => _make;
+        set => _make = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public bool IsLuxury
+    {
+        get => _isLuxury;
+        
+    }
+}
