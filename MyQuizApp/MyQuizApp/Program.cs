4d41263988d03fce0b23dd22be2751665c5ed2c2
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2024-12-23 09:53:19 -0500

    created new MyQuizApp solution, and Question.cs class within it

---
+++ MyQuizApp/MyQuizApp/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace MyQuizApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ MyQuizApp/MyQuizApp/Question.cs
@@ -0,0 +1,21 @@
+namespace MyQuizApp;
+
+public class Question
+{
+    public string QuestionText { get; set; }
+    public string[] Answers { get; set; }
+    public int CorrectAnswerIndex { get; set; }
+
+    public Question(string questionText, string[] answers, int correctAnswerIndex)
+    {
+        QuestionText = questionText;
+        Answers = answers;
+        CorrectAnswerIndex = correctAnswerIndex;
+    }
+
+    public bool IsCorrectAnswer(int choice)
+    {
+        return CorrectAnswerIndex == choice;
+    }
+    
+}
