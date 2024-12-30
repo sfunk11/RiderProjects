e8526741fb1eda5aa9b5deac2f90f0ea9ed6db14
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2024-12-30 08:39:19 -0500

    removed some noise from git for quiz app

--- ListsApp/ListsApp/Program.cs
+++ ListsApp/ListsApp/Program.cs
@@ -4,7 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<string> list = new List<string>();
+        List<string> colors = new List<string>();
+        colors.Add("red");
+        colors.Add("green");
+        colors.Add("blue");
+        colors.Add("red");
+
+        Console.WriteLine("Current colors in the list:");
+        foreach (string color in colors)
+        {
+            Console.WriteLine(color);
+        }
+        
+        // .Remove() removes first occurence only
+        colors.Remove("red");
+        Console.WriteLine("Current colors in the list:");
+        foreach (string color in colors)
+        {
+            Console.WriteLine(color);
+        }
         
         
     }
