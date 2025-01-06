d1fb46b2b0bb669b0379cda89f8a57a87b01c58b
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2025-01-06 10:50:40 -0500

    updated to include example of multiple subclasses

--- InheritanceApp/InheritanceApp/Program.cs
+++ InheritanceApp/InheritanceApp/Program.cs
@@ -4,6 +4,89 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Dog myDog = new Dog();
+        myDog.Eat();
+        myDog.MakeSound();
+
+        Employee joe = new Employee("Joe", 25, "Representative", 12345);
     }
+
+    //Base Class (Superclass or parent)
+    class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Eating...");
+        }
+//use "virtual" keyword to make a method overridable
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Making a sound...");
+        }
+    }
+
+    //Child Class (derived class or subclass)
+    class Dog : Animal
+    {
+        
+        public override void MakeSound()
+        {
+            Console.WriteLine("Barking...");
+        }
+        
+        
+    }
+//Subclasses can inherit from subclasses as well as base classes
+    class Corgi : Dog
+    {
+        public void Herd()
+        {
+            Console.WriteLine("Herding...");
+        }
+    }
+
+    //Multiple subclasses can inherit from single base class (Heirarchical inheritance)
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meowing...");
+        }
+    }
+    
+    //A class can only inherit from one class, but can be mimicked by inheriting from interfaces rather than classes
+
+    public class Person
+    {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+            Console.WriteLine($"Hello, {name} {age}");
+        }
+    }
+
+    public class Employee : Person
+    {
+        public string JobTitle { get; private set; }
+        public int EmployeeId { get; private set; }
+        // need base in signature for parent constructor parameters
+        public Employee(string name, int age, string jobTitle, int employeeId) : base(name,age)
+        {
+            JobTitle = jobTitle;
+            EmployeeId = employeeId;
+            
+        }
+    }
+
+    public class Manager : Employee
+    {
+        public Manager(string name, int age, string jobTitle, int employeeId) : base(name, age, jobTitle, employeeId)
+        {
+        }
+    }
+    
 }
