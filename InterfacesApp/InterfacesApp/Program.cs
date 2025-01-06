3d5d40aabea053a40748f3ebc3b0ef1ba6b380e6
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2025-01-06 11:55:50 -0500

    added InterfacesApp for Polymorphism chapter

---
+++ InterfacesApp/InterfacesApp/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace InterfacesApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       Dog dog = new Dog();
+       dog.MakeNoise();
+       dog.Eat("treats");
+       
+       Cat cat = new Cat();
+       cat.MakeNoise();
+       cat.Eat("meat");
+    }
+}
+
+//interfaces start with "I"
+public interface IAnimal
+{
+    void MakeNoise();
+    void Eat(string food);
+}
+
+public class Dog : IAnimal
+{
+    public void Eat(string food)
+    {
+        Console.WriteLine($"Dog is eating {food}");
+    }
+
+    public void MakeNoise()
+    {
+        Console.WriteLine("Bark!");
+    }
+}
+
+public class Cat : IAnimal
+{
+    public void Eat(string food)
+    {
+        Console.WriteLine($"Cat is eating {food}");
+    }
+
+    public void MakeNoise()
+    {
+        Console.WriteLine("Meow!");
+    }
+}
