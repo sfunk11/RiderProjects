da600b60b613517d685ffcfdc34705ee44cf1818
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2025-01-07 09:57:27 -0500

    Completed Polymorphism / Interfaces chapter

--- DependencyInjectionApp/DependencyInjectionApp/Program.cs
+++ DependencyInjectionApp/DependencyInjectionApp/Program.cs
@@ -7,22 +7,36 @@ public class Hammer
         Console.WriteLine("Hammering nails");
     }
 }
-
-public class Builder
+//Interface injection is a more complex version of setter injection
+public class Builder : IToolUser
 {
-    private Hammer _hammer;
-    private Saw _saw;
+   private Hammer _hammer;
+   private Saw _saw;
+  //Setter injection 
+   // public Hammer Hammer { get; set; }
+   // public Saw Saw { get; set; }
+   public void SetHammer(Hammer hammer)
+   {
+       _hammer = hammer;
+   }
 
-    public Builder(Hammer hammer, Saw saw)
-    {
-        _hammer = hammer;
-        _saw = saw;
-    }
+   public void SetSaw(Saw saw)
+   {
+      _saw = saw;
+   }
+  
+//Constructor injection
+    // public Builder(Hammer hammer, Saw saw)
+    // {
+    //     _hammer = hammer;
+    //     _saw = saw;
+    // }
 
     public void Build()
     {
         _hammer.Use();
         _saw.Use();
+        
         Console.WriteLine("House built");
     }
 }
@@ -34,7 +48,36 @@ public class Saw
         Console.WriteLine("Sawing wood.");
     }
 }
+//Interface injection
+public interface IToolUser
+{
+    void SetHammer(Hammer hammer);
+    void SetSaw(Saw saw);
+}
 
+public interface IPrintable
+{
+    void Print();
+}
+
+public interface IScannable
+{
+    void Scan();
+}
+
+//Multiple Interface Inheritance example
+public class MultiFunctionPrinter : IPrintable, IScannable
+{
+    public void Scan()
+    {
+        Console.WriteLine("Scanning...");
+    }
+
+    public void Print()
+    {
+        Console.WriteLine("Printing...");
+    }
+}
 
 class Program
 {
@@ -42,7 +85,15 @@ class Program
     {
         Hammer myHammer = new Hammer();
         Saw mySaw = new Saw();
-        Builder myBuilder = new Builder(myHammer, mySaw);
+        Builder myBuilder = new Builder();
+        // myBuilder.Hammer = myHammer;
+        // myBuilder.Saw = mySaw;
+        myBuilder.SetHammer(myHammer);
+        myBuilder.SetSaw(mySaw);
         myBuilder.Build();
+        
+        MultiFunctionPrinter myMultiFunctionPrinter = new MultiFunctionPrinter();
+        myMultiFunctionPrinter.Print();
+        myMultiFunctionPrinter.Scan();
     }
 }
