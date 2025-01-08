0c623f8ccf3782ae549db6f14587e8d4f7ed3802
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2025-01-08 08:40:56 -0500

    initial commit for Delegates and Events

---
+++ DelegatesAndEvents/DelegatesAndEvents/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Concurrent;
+
+namespace DelegatesAndEvents;
+
+public delegate void LogHander(string message);
+
+public class Logger
+{
+    public void LogToConsole(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+    public void LogToFile(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
+
+class Program
+{
+    
+    //1, Declaration ( you can declare a delegate outside of any class if needed for access)
+    public delegate void Notify(string message);
+    
+    
+    static void Main(string[] args)
+    {
+        //2. Instantiation:
+        Notify notifyDelegate = ShowMessage;
+        
+        //3. invokation
+        notifyDelegate("Hello Delegate!");
+
+        Logger myLogger = new Logger();
+        LogHander logDelegate = myLogger.LogToConsole;
+        logDelegate("Log to Console");
+        
+        logDelegate = myLogger.LogToFile;
+        logDelegate("Log to File");
+        
+
+    }
+
+    static void ShowMessage(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
