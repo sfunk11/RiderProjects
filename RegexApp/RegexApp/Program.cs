529a533ce54c0ae91f4a3c303a439b875f29734b
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2025-01-13 09:34:33 -0500

    added regex matching

--- RegexApp/RegexApp/Program.cs
+++ RegexApp/RegexApp/Program.cs
@@ -1,9 +1,25 @@
-﻿namespace RegexApp;
+﻿using System.Text.RegularExpressions;
+
+namespace RegexApp;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        string pattern = @"(\d+)";
+        Regex regex = new Regex(pattern);
+
+        string text = "Hi there, ny number is 12314";
+        
+        MatchCollection matches = regex.Matches(text);
+        
+        Console.WriteLine($"Matches count: {matches.Count}");
+
+        foreach (Match match in matches)
+        {
+            GroupCollection groups = match.Groups;
+            Console.WriteLine(groups[0].Value);
+        }
     }
 }
